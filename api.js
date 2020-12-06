a3f955c926de29162a481a0eca12a1355d170d37
Elliot Plant <elliotaplant@gmail.com>
2020-12-05 20:02:02 -0800

    No headers in API

--- api.js
+++ api.js
@@ -1,39 +1,12 @@
 function makePaste(body) {
   const url = 'https://remote-clipboard.netlify.app/.netlify/functions/paste';
-  let options = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body,
-  };
-
-  if (location.host !== 'remote-clipboard.netlify.app') {
-    options = {
-      ...options,
-      // mode: 'no-cors', // no-cors, *cors, same-origin
-      credentials: 'omit', // include, *same-origin, omit
-    };
-  }
-
+  const options = { method: 'POST', body };
   return fetch(url, options).then(r => r.json());
 }
 
 function getPaste() {
-  let url = '/.netlify/functions/copy';
-  let options = {};
-
-  // if (location.host !== 'remote-clipboard.netlify.app') {
-  url = 'https://remote-clipboard.netlify.app' + url;
-
-  options = {
-    // mode: 'no-cors', // no-cors, *cors, same-origin
-    credentials: 'omit', // include, *same-origin, omit
-  };
-  // }
-  console.log('url', url);
-
-  return fetch(url, options).then(resp => resp.text());
+  return fetch('https://remote-clipboard.netlify.app/.netlify/functions/copy')
+    .then(resp => resp.text());
 }
 
 window.makePaste = makePaste;
